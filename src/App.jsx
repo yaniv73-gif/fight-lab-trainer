ff4843135c180dddb34af4378fe5c42aff50e794
Yaniv <yaniv73@gmail.com>
2026-06-30 14:39:02 +0300

    Fix BrowserRouter basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
